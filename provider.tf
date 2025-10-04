718a154e65f5e39688045d2c434d6625fdeab727
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2025-10-04 12:05:51 -0300

    fix: backed s3 key value for terraform state

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "baiters-burger-infra"
-    key    = "terraform/infra/state.tfstate"
+    key    = "terraform/database/state.tfstate"
     region = "us-east-1"
   }
   required_providers {
